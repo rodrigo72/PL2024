14f9f524435c431270d7c26f82597aa1780612b3
rodrigo72 <a100706@alunos.uminho.pt>
2024-03-01 09:38:50 +0000

    correçao tpc3

--- TPC3/main.py
+++ TPC3/main.py
@@ -4,19 +4,21 @@ import sys, re
 def main():
     
     total = 0
-    on = False
+    on = True
     file = sys.stdin.read()
-    matches = re.findall(r'(?i)(on|off)(.*?)(?=on|off|\Z)', file, re.DOTALL)
+    matches = re.findall(r'(?i)(^|on|off|\=)(.*?)(?=on|off|\=|\Z)', file, re.DOTALL)
 
     for match in matches:        
-        on = True if match[0].lower() == 'on' else False
-        
-        if on:
-            numbers = re.findall(r'\d+', match[1], re.DOTALL)
-            total += sum(map(int, numbers))
-            
-        if re.findall(r'\=', match[1], re.DOTALL):
+        if match[0].lower() == 'off':
+            on = False
+        elif match[0] == '=':
             print(f'Soma = {total}')
+        elif match[0].lower() == 'on':
+            on = True
+
+        if on:
+            numbers = re.findall(r'(?:\D|^)(\d+)(?=\D|\Z)', match[1], re.DOTALL)
+            total += sum(map(int, numbers))
                         
 
 if __name__ == "__main__":
